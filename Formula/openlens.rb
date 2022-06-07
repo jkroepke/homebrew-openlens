a3f60465aa448f7cf5a091226a584db8c06feb66
Jan-Otto Kröpke <mail@jkroepke.de>
2022-06-07 21:50:16 +0200

    Setup TAP (#1)

--- Formula/openlens.rb
+++ Formula/openlens.rb
@@ -1,5 +1,5 @@
 class Openlens < Formula
-  desc "The Kubernetes IDE"
+  desc "Kubernetes IDE"
   homepage "https://github.com/lensapp/lens"
   url "https://github.com/lensapp/lens.git",
       tag:      "v5.5.3",
@@ -19,29 +19,33 @@ class Openlens < Formula
   def install
     # Don't dirty the git tree
     rm_rf ".brew_home"
-
-    ENV["ELECTRON_BUILDER_EXTRA_ARG"] = "--macos dir" if OS.mac?
-
+    ENV["ELECTRON_BUILDER_EXTRA_ARGS"] = OS.mac? ? "--macos dir" : "--linux dir"
     system "make", "build"
 
     if OS.mac?
       prefix.install "dist/mac/OpenLens.app"
       bin.write_exec_script prefix/"OpenLens.app/Contents/MacOS/OpenLens"
+    else
+      prefix.install "dist/linux-unpacked"
+      bin.write_exec_script prefix/"linux-unpacked/open-lens"
     end
   end
 
   def caveats
-    <<~EOS
-      To start OpenLens, from a terminal run
-      "#{prefix}/OpenLens.app/Contents/MacOS/OpenLens".
+    if OS.mac?
+      <<~EOS
+        To start OpenLens, from a terminal run
+        "#{prefix}/OpenLens.app/Contents/MacOS/OpenLens".
 
-      Alternatively, run
-      ln -sfn "#{prefix}/OpenLens.app" /Applications/OpenLens.app
-      to start OpenLens from Spotlight or Finder.
-    EOS
+        Alternatively, run
+        ln -sfn "#{prefix}/OpenLens.app" /Applications/OpenLens.app
+        to start OpenLens from Spotlight or Finder.
+      EOS
+    end
   end
 
   test do
-    assert_path_exists prefix/"OpenLens.app/Contents/MacOS/OpenLens" if OS.mac?
+    binary_path = OS.mac? ? prefix/"OpenLens.app/Contents/MacOS/OpenLens" : prefix/"dist/linux-unpacked/open-lens"
+    assert_predicate binary_path, :executable?
   end
 end
