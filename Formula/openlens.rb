c95c45e35de55ba9653c3d1c7fec63b99ebe447c
Jan-Otto Kröpke <github@jkroepke.de>
2022-07-28 12:49:52 +0000

    openlens 6.0.0

--- Formula/openlens.rb
+++ Formula/openlens.rb
@@ -2,8 +2,8 @@ class Openlens < Formula
   desc "Kubernetes IDE"
   homepage "https://github.com/lensapp/lens"
   url "https://github.com/lensapp/lens.git",
-      tag:      "v5.5.4",
-      revision: "d2f586e714889a1c3c8191376d1fac5f2d70aace"
+      tag:      "v6.0.0",
+      revision: "9bc0e8951bbcfe153ac796fa661de2b4bc6a18ce"
   license "MIT"
   head "https://github.com/lensapp/lens.git", branch: "main"
 
